989889bf50bb8afede79d25547433907961d5d39
Stanley Liu <sliu1993@hotmail.co.uk>
2021-12-23 12:21:09 +0000

    style page and login form as well as stylelint config

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,66 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="overlay"></div>
+  <div class="wrapper">
+    <div class="wrapper__content">
+      <h1 class="app-title">
+        Spotify
+        <span class="color-white">Vuesualiser</span>.
+      </h1>
+      <!-- Div to hold login screen, visualisations and more -->
+      <div class="wrapper__components">
+        <login-form></login-form>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<script setup>
+  import LoginForm from './components/LoginForm.vue';
+</script>
+
+<style lang="scss" scoped> 
+.overlay {
+  background-image: url("../src/assets/images/background.svg");
+  background-size: cover;
+  opacity: 0.8;
+  filter: blur(2px) grayscale(30%);
+
+  position: fixed;
+  height: 100%;
+  width: 100%;
+
+  z-index: -1;
+}
+
+.wrapper {
+  z-index: 1;
+
+  height: 100%;
+  width: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &__content {
+    width: 1000px;
+    height: 800px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  &__components {
+    // todo Temporary guides put in place - remove later
+    border: 1px solid black;
+    border-radius: 2px;
+    
+    margin-top: 20px;
+
+    width: 100%;
+    height: 100%;
+  }
 }
 </style>
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="oauth-wrapper">
+    <form class="login-form" @submit.prevent="authenticate">
+      <h4 class="login-form__title">Sign in through Spotify</h4>
+      <div class="login-form__fields">
+        <label for="username" class="login-form__label">Username</label>
+        <input class="login-form__field" type="text" name="Username" id="username" v-model.trim="username"/>
+        <label for="password" class="login-form__label">Password</label>
+        <input class="login-form__field" type="password" name="Password" id="password" v-model.trim="password"/>
+        <button type="submit" :disabled="username === '' || password === ''">Log In Via OAuth</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  const username = ref('')
+  const password = ref('')
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/stylesheets/variables.scss";
+.oauth-wrapper {
+  background-color: black;
+  border: 1px solid white;
+  border-radius: 6px;
+
+  margin: 80px auto;
+
+  height: 60%;
+  width: 400px;
+}
+
+.login-form {
+  color: white;
+
+  &__title {
+    margin-top: 40px;
+    margin-bottom: 10px;
+    text-align: center;
+  }
+
+  &__fields {
+    padding: 30px;
+  }
+
+  &__label {
+    font-family: $font-body;
+    text-align: left;
+  }
+
+  &__field {
+    border: 1px solid black;
+    border-radius: 2px;
+
+    margin-top: 10px;
+    margin-left: auto;
+    margin-bottom: 20px;
+    margin-right: auto;
+    padding: 10px;
+
+    width: 100%;
+
+    display: block;
+
+    &:last-of-type {
+      margin-bottom: 40px;
+    }
+  }
+}
+
+button {
+  background-color: $spotify-green;
+  border-radius: 3px;
+  color: white;
+
+  font-family: $font-body;
+  font-weight: bold;
+  font-size: 20px;
+  text-transform: uppercase;
+
+  margin: 0 auto;
+  padding: 8px 10px;
+
+  display: block;
+
+  transition: 0.5s;
+
+  &:hover:not(:disabled) {
+    background-color: green;
+  }
+
+  &:disabled {
+    opacity: 0.6;
+  }
+}
+</style>
